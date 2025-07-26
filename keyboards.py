3d87b6730d72bbb569ff4e2578215e3f2acba051
soc321 <91270627+soc321@users.noreply.github.com>
2025-07-26 21:05:46 +0700

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -3,15 +3,13 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 def main_keyboard(is_admin=False):
     kb = [
         [KeyboardButton("💼 Đầu Tư"), KeyboardButton("💸 Rút Lãi")],
-        [KeyboardButton("👤 Tài Khoản")]
+        [KeyboardButton("💳 Nạp Tiền"), KeyboardButton("👤 Tài Khoản")]
     ]
     if is_admin:
         kb.append([KeyboardButton("⚙️ Admin Panel")])
     return ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
 
-admin_panel_kb = ReplyKeyboardMarkup(
-    keyboard=[
-        [KeyboardButton("📥 Danh Sách Nạp"), KeyboardButton("📤 Danh Sách Rút")],
-        [KeyboardButton("🔙 Quay Lại")]
-    ], resize_keyboard=True
-)
+admin_panel_kb = ReplyKeyboardMarkup(resize_keyboard=True, keyboard=[
+    [KeyboardButton("📥 Danh Sách Nạp"), KeyboardButton("📤 Danh Sách Rút")],
+    [KeyboardButton("🔙 Quay Lại")]
+])
