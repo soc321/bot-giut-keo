e51a11ae0078fa3952c873094f224c8d0c5d0c6e
soc321 <91270627+soc321@users.noreply.github.com>
2025-07-26 21:15:38 +0700

    Create states.py

---
+++ states.py
@@ -0,0 +1,10 @@
+from aiogram.dispatcher.filters.state import State, StatesGroup
+
+class InvestmentStates(StatesGroup):
+    waiting_for_package_choice = State()
+
+class WithdrawStates(StatesGroup):
+    waiting_for_amount = State()
+
+class DepositStates(StatesGroup):
+    waiting_for_amount = State()
