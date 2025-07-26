bc839b38ccc63c61c01d41a16ed3d7f24684826d
soc321 <91270627+soc321@users.noreply.github.com>
2025-07-26 23:38:42 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,2 @@
-TOKEN = "8121092898:AAG2FjMFjP1rjBWkaSSl_PpiMGee0StjuGg"
+API_TOKEN = "8121092898:AAG2FjMFjP1rjBWkaSSl_PpiMGee0StjuGg"
 ADMIN_IDS = ["6730559072"]  # Thay bằng Telegram user_id của 
-
-BOT_BANK_NAME = "MB Bank"
-BOT_BANK_NUMBER = "1234567890"
-
-MIN_WITHDRAW = 7000
-MAX_WITHDRAW = 500000
-
-INVESTMENTS = [
-    {"name": "Gói Trải Nghiệm", "amount": 5000, "daily": 2000, "days": 7},
-    {"name": "Gói Tiết Kiệm", "amount": 20000, "daily": 5000, "days": 7},
-    {"name": "Gói Lợi Nhuận", "amount": 50000, "daily": 5000, "days": 15},
-    {"name": "Gói Siêu Lãi", "amount": 100000, "daily": 5000, "days": 30},
-]
