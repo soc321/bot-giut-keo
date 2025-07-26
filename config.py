8b93bba650d0c84822e1a3f59f3acc8f6e1d0ab0
soc321 <91270627+soc321@users.noreply.github.com>
2025-07-26 21:05:08 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,14 @@
 TOKEN = "8121092898:AAG2FjMFjP1rjBWkaSSl_PpiMGee0StjuGg"
 ADMIN_IDS = ["6730559072"]  # Thay bằng Telegram user_id của admin
+
+INVESTMENTS = [
+    {"amount": 5000, "daily": 2000, "days": 7, "name": "Gói Trải Nghiệm"},
+    {"amount": 20000, "daily": 5000, "days": 7},
+    {"amount": 50000, "daily": 5000, "days": 15},
+    {"amount": 100000, "daily": 5000, "days": 30},
+]
+
+MIN_WITHDRAW = 7000
+MAX_WITHDRAW = 500000
+
+USER_DATA_FILE = "users_data.json"
