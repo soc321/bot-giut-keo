8936bafb83f4d1391776c4f8a2164e3d0eb0598f
soc321 <91270627+soc321@users.noreply.github.com>
2025-07-27 00:16:57 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,12 @@
-API_TOKEN = "8121092898:AAG2FjMFjP1rjBWkaSSl_PpiMGee0StjuGg"
+TOKEN = "8121092898:AAG2FjMFjP1rjBWkaSSl_PpiMGee0StjuGg"
 ADMIN_IDS = ["6730559072"]  # Thay bằng Telegram user_id của 
 
-MIN_WITHDRAW = 7000
+MIN_WITHDRAW = 1000
 MAX_WITHDRAW = 500000
+
+DEPOSIT_PACKAGES = {
+    "Gói 1": {"amount": 10000, "daily_rate": 0.01},
+    "Gói 2": {"amount": 50000, "daily_rate": 0.015},
+    "Gói 3": {"amount": 100000, "daily_rate": 0.02},
+    "Gói 4": {"amount": 500000, "daily_rate": 0.03},
+}
