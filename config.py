417064418714b4d795362087a31a21271326ec70
soc321 <91270627+soc321@users.noreply.github.com>
2025-07-26 21:17:56 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 TOKEN = "8121092898:AAG2FjMFjP1rjBWkaSSl_PpiMGee0StjuGg"
 ADMIN_IDS = ["6730559072"]  # Thay bằng Telegram user_id của admin
 
+# Gói đầu tư: amount = số tiền mua, daily = lãi/ngày, days = số ngày
 INVESTMENTS = [
-    {"amount": 5000, "daily": 2000, "days": 7, "name": "Gói Trải Nghiệm"},
+    {"name": "Gói Trải Nghiệm", "amount": 5000, "daily": 2000, "days": 7},
     {"amount": 20000, "daily": 5000, "days": 7},
     {"amount": 50000, "daily": 5000, "days": 15},
     {"amount": 100000, "daily": 5000, "days": 30},
@@ -10,5 +11,3 @@ INVESTMENTS = [
 
 MIN_WITHDRAW = 7000
 MAX_WITHDRAW = 500000
-
-USER_DATA_FILE = "users_data.json"
