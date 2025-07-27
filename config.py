372bdd6b11c0010ae5d6a387f9822b227ab091bf
soc321 <91270627+soc321@users.noreply.github.com>
2025-07-27 07:08:09 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("8121092898:AAG2FjMFjP1rjBWkaSSl_PpiMGee0StjuGg")
-ADMIN_IDS = [int(i) for i in os.getenv("6730559072", "").split(",") if i]
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_IDS = [int(i) for i in os.getenv("ADMIN_IDS", "").split(",") if i]
 
 USERS_FILE = "users.json"
