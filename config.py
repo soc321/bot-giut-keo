d28195bbd3e7c46bda4bf14d5bc540483302f37f
soc321 <91270627+soc321@users.noreply.github.com>
2025-07-27 07:07:29 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,9 @@
-TOKEN = "8121092898:AAG2FjMFjP1rjBWkaSSl_PpiMGee0StjuGg"
-ADMIN_IDS = ["6730559072"]  # Thay bằng Telegram user_id của 
+import os
+from dotenv import load_dotenv
 
-INVESTMENTS = [
-    {"name": "Gói 1", "amount": 10000, "daily": 1000, "days": 5},
-    {"name": "Gói 2", "amount": 20000, "daily": 2500, "days": 5},
-    {"name": "Gói 3", "amount": 50000, "daily": 7000, "days": 5},
-    {"name": "Gói VIP", "amount": 100000, "daily": 15000, "days": 5},
-]
+load_dotenv()
 
-BOT_BANK_NAME = "MB Bank"
-BOT_BANK_NUMBER = "0123456789"
+BOT_TOKEN = os.getenv("8121092898:AAG2FjMFjP1rjBWkaSSl_PpiMGee0StjuGg")
+ADMIN_IDS = [int(i) for i in os.getenv("6730559072", "").split(",") if i]
 
-MIN_WITHDRAW = 7000
-MAX_WITHDRAW = 500000
+USERS_FILE = "users.json"
