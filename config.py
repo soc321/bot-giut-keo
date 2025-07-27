aad03aa35afeb29960cf9a0746fcb98df32615a5
soc321 <91270627+soc321@users.noreply.github.com>
2025-07-27 11:56:44 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,14 @@
-import os
-from dotenv import load_dotenv
+TOKEN = "8121092898:AAG2FjMFjP1rjBWkaSSl_PpiMGee0StjuGg"
+ADMIN_IDS = ["6730559072"]  # ID admin
+BOT_BANK_NAME = "MB Bank"
+BOT_BANK_NUMBER = "0123456789"
+MIN_WITHDRAW = 7000
+MAX_WITHDRAW = 500000
 
-load_dotenv()
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_IDS = [int(i) for i in os.getenv("ADMIN_IDS", "").split(",") if i]
-
-USERS_FILE = "users.json"
+# Các gói đầu tư
+INVESTMENTS = [
+    {"name": "Gói 1", "amount": 10000, "daily": 2000, "days": 3},
+    {"name": "Gói 2", "amount": 20000, "daily": 3500, "days": 4},
+    {"name": "Gói 3", "amount": 30000, "daily": 5500, "days": 5},
+    {"name": "Gói 4", "amount": 50000, "daily": 10000, "days": 7}
+]
